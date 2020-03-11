1b600388798b24fdba33bd6fd12a298bac28e485
Brady Snuggs <dronebug360@gmail.com>
2020-03-11 10:07:42 -0600

    setup async funcs, state, and utility components.

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
-import React,{useState} from 'react';
+import React,{useState, useEffect} from 'react';
 import {View, 
         Text,
         Animated,
         StyleSheet,
       } from 'react-native'
-
+import {List} from 'react-native-paper';
 
 
 
 const App = () => {
   
-  
+// useEffect(() => {
+
+// },[])
+
+async function getUsersList = () => {
+
+}
+
+async function setUsersList = () => {
+
+}
+
+const [list, setList] = useState({
+  todoList: []
+})
+
   return(
     <View
       style={styles.container}
     >
       <Text>ZooWho ToDo App</Text>
+      <TaskContainer
+
+      />
 
     </View>
   )
